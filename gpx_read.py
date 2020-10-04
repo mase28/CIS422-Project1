70e3d80eef2b3b501bc20f59fda1b4ab6cdfb8d2
mase28 <mase28@github.com>
2020-10-04 15:27:18 -0700

    Program that reads gpx files

---
+++ gpx_read.py
@@ -0,0 +1,18 @@
+import gpxpy
+import gpxpy.gpx
+
+#Opens gpx file and stores coordinates in a list of tuples
+def get_coordinates(file: str) -> list:
+	gpx_file = open(file, 'r')
+	gpx = gpxpy.parse(gpx_file)
+	result = []
+
+	for track in gpx.tracks:
+		for segment in track.segments:
+			for point in segment.points:
+				result.append((point.latitude, point.longitude))
+
+	gpx_file.close()
+
+	return result
+
