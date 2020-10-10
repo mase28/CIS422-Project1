ad359ab4f06e284b0cb0c2ca17d3a59580f1fca7
mase28 <mase28@github.com>
2020-10-09 17:47:33 -0700

    Main now compares json to gpx coordinates

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-import re
-import json
-from gpx_read import get_coordinates
-from create_route import get_path
+from gpx_read import get_coordinates_gpx
+from create_route import get_path, get_coordinates_json
+from compare_coord_files import comparingResult
 
 if __name__ == "__main__":
-	ls = get_coordinates("gpx_file.gpx")
+	ls = get_coordinates_gpx("gpx_file.gpx")
 
 	ret = get_path([ls[0], ls[len(ls)-1]])
 
-	js = json.loads(ret)
-	for keys in js:
-		if keys == "features":
-			temp = js["features"][0]
-			for key in temp:
-				if key == "geometry":
-					print(temp[key]["coordinates"])
+	coords = get_coordinates_json(ret)
+
+	for i in range(len(coords)):
+		if comparingResult(ls[i], coords[i]):
+			print(f"Coordinate in gpx: {ls[i]} Coordinate in json: {coords[i]}. These line up.")
+		else:
+			print(f"Coordinate in gpx: {ls[i]} Coordinate in json: {coords[i]}. These do not line up.")
+
 	
