9998818a51204ea1cb304af80930080cbfaaeeb0
Shoshanah <49079700+Shoshanah@users.noreply.github.com>
2020-10-19 01:48:57 -0700

    Update License.py

--- License.py
+++ License.py
@@ -1,5 +1,5 @@
 """
-Copyright 2020 Mason Olson
+Copyright 2020 Mason Olsen
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
